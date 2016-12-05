f002e1a3b18cfd9f2761370056f242cef4bc7ccc
Francesco Grossetti <francesco.grossetti@polimi.it>
2016-12-05 10:04:50 +0100

    formalized utility funcs like compute_pval_max and final_fit which are now internal

--- R/utils.R
+++ R/utils.R
@@ -1,74 +1,79 @@
-#' Utility to count missing data in a \code{matrix}, \code{data.frame} or 
-#' \code{data.table}.
-#'
-#' The function returns the sum of missing data detected in each column.
-#'
-#' @param data A \code{matrix}, \code{data.frame} or \code{data.table}.
-#' 
-#' @param prop Compute the column proportion of missing data.
-#' 
-#' @return Print the results in console.
-#' 
-#' @author Francesco Grossetti \email{francesco.grossetti@@gmail.com}.
-#' @export
-
-count_na = function( data, prop = TRUE ) {
-  print( apply( data, 2, function( x ) sum( is.na( x ) ) ) )
-  if ( prop ) {
-    print( apply( data, 2, 
-                  function( x ) round( sum( is.na( x ) ) / length( x ) * 100, 2 ) ) )
+compute_pval_max = function( obj ) {
+  z = obj
+  r = z$residuals
+  n = length( r )
+  rss = sum( r^2 )
+  rdf = z$df.residual
+  resvar = rss / rdf
+  sigma = sqrt( resvar )
+  Qr = z$qr
+  p = z$rank
+  p1 = 1L:p
+  R = chol2inv( Qr$qr[ p1, p1, drop = FALSE ] )
+  se = sqrt( diag( R ) * resvar )
+  est = z$coefficients[ Qr$pivot[ p1 ] ]
+  tval = est/se
+  pval = 2 * pt( abs( tval ), rdf, lower.tail = FALSE )
+  if ( any( names( z$coefficients ) %in% "(Intercept)" ) ) { 
+    pval_max = names( which.max( pval[ !names( pval ) %in% "(Intercept)" ] ) )
+  } else {
+    pval_max = names( which.max( pval ) )
   }
-}
-
-#' Complete a dataset after \code{\link[mice]{mice}} has been run.
-#' 
-#' Utility to define a non-repeated dataset after a missing imputation algorithm 
-#' with the function \code{\link[mice]{mice}} has been run and the associated 
-#' complete procedure through \code{\link[mice]{complete}} has been carried out.
-#'
-#' @param data A \code{data.table} or a \code{data.frame} (see 'Details').
-#' @param data_key The keying variable in the \code{data.table}.
-#' 
-#' @details The function acts on numeric variables only and takes a column mean
-#' by considering all the simulations detected given by \code{.imp}. This should
-#' ensure consistent results as given by \code{\link[mice]{pool}}
-#' When a \code{data.frame} is passed, \code{def_complete} internally
-#' converts it to a \code{data.table} but still returns a \code{data.frame}.
-#' 
-#' @return Either a \code{data.table} or a \code{data.frame}.
-#' 
-#' @author Francesco Grossetti \email{francesco.grossetti@@gmail.com}.
-#' @export
-#' @importFrom data.table data.table
-#' @importFrom data.table setkeyv
-#' @importFrom data.table setDT
-#' @importFrom data.table setDF
-
-def_complete = function( data, data_key ) {
-
-  if ( !inherits( data, "data.table" ) ) {
-    message( "converting ", data, " to a data.table" )
-    setDT( data )
+  if ( alpha > pval[ pval_max ] && pval_max != "(Intercept)" && !exists( "X_red" ) ) {
+    cat( "---\n" )
+    message( "alpha is too high to start the reduction. Please lower the significance value" )
+    cat( "---\n" )
+  } else if ( alpha > pval[ pval_max ] && pval_max != "(Intercept)" && exists( "X_red" ) ) {
+    cat( "---\n" )
+    message( "reduction procedure completed with alpha = ", alpha )
+    cat( "---\n" )
   }
-
-  data_key = as.character( substitute( list( data_key ) )[ -1L ] )
-  numeric_vars = sapply( data, is.numeric )
-  average = data[ , lapply( .SD, mean ), by = eval( data_key ),
-                  .SDcols = numeric_vars ]
-  factor_vars = sapply( data, is.factor )
-  factor_data = data[ .imp == 1, .SD, .SDcols = factor_vars ]
-  setkeyv( average, data_key )
-  setkeyv( factor_data, data_key )
-  finale = average[ factor_data ]
-  finale[ , .id := NULL ]
-  finale[ , .imp := NULL ]
+  lm.results = matrix( 0, nrow = z$rank, ncol = 4, byrow = TRUE ) 
+  lm.results = cbind( est, se, tval, pval )
+  colnames( lm.results ) = c( "Estimate", "Std. Error", "t value", "Pr(>|t|)" ) 
   
-  if ( !inherits( data, "data.table" ) ) {
-    message( "converting output to a data.frame" )
-    setDF( finale )
-  }
-  return( invisible( finale ) )
+  return( list( pval, pval_max ) )
 }
 
 
-
+final_fit = function( obj ) {
+  digits = 5
+  z = obj
+  r = z$residuals
+  n = length( r )
+  p = z$rank
+  rss = sum( r^2 )
+  rdf = z$df.residual
+  resvar = rss / rdf
+  sigma = sqrt( resvar )
+  f = z$fitted.values
+  Qr = z$qr
+  p1 = 1L:p
+  R = chol2inv( Qr$qr[ p1, p1, drop = FALSE ] )
+  if( any( names( z$coefficients ) %in% "(Intercept)" ) ) {
+    mss = sum( ( f - mean( f ) )^2 ) 
+    df.int = 1L
+  } else {
+    mss = sum( f^2 )
+    df.int = 0L
+  }
+  r.squared = mss / ( mss + rss )
+  adj.r.squared = 1 - ( 1 - r.squared ) * ( ( n - df.int ) / rdf )
+  fstatistic = c( value = ( mss / ( p - df.int ) ) / resvar, 
+                  numdf = p - df.int, 
+                  dendf = rdf )
+  se = sqrt( diag( R ) * resvar )
+  est = z$coefficients[ Qr$pivot[ p1 ] ]
+  tval = est/se
+  pval = 2 * pt( abs( tval ), rdf, lower.tail = FALSE )
+  lm.results = matrix( 0, nrow = z$rank, ncol = 4, byrow = TRUE ) 
+  lm.results = cbind( est, se, tval, pval )
+  colnames( lm.results ) = c( "Estimate", "Std. Error", "t value", "Pr(>|t|)" ) 
+  
+  return( invisible( list( coef = lm.results, residuals = r, 
+                           fitted.values = f, sigma = sigma, 
+                           r.squared = r.squared, 
+                           adj.r.squared = adj.r.squared, 
+                           fstatistic = fstatistic,
+                           rdf = rdf ) ) )
+}
