4a184924be92a3a69eebc40e5654ac8cac2c2fbc
Francesco Grossetti <francesco.grossetti@polimi.it>
2016-12-05 10:40:54 +0100

    compute_pval_max and final_fit are still defined under reduction scope

--- R/reduction.R
+++ R/reduction.R
@@ -55,6 +55,86 @@ reduction = function( model, ... ) {
 
 reduction.lm = function( model, data, alpha = 0.05, intercept = TRUE ) {
   
+  compute_pval_max = function( obj ) {
+    z = obj
+    r = z$residuals
+    n = length( r )
+    rss = sum( r^2 )
+    rdf = z$df.residual
+    resvar = rss / rdf
+    sigma = sqrt( resvar )
+    Qr = z$qr
+    p = z$rank
+    p1 = 1L:p
+    R = chol2inv( Qr$qr[ p1, p1, drop = FALSE ] )
+    se = sqrt( diag( R ) * resvar )
+    est = z$coefficients[ Qr$pivot[ p1 ] ]
+    tval = est/se
+    pval = 2 * pt( abs( tval ), rdf, lower.tail = FALSE )
+    if ( any( names( z$coefficients ) %in% "(Intercept)" ) ) { 
+      pval_max = names( which.max( pval[ !names( pval ) %in% "(Intercept)" ] ) )
+    } else {
+      pval_max = names( which.max( pval ) )
+    }
+    if ( alpha > pval[ pval_max ] && pval_max != "(Intercept)" && !exists( "X_red" ) ) {
+      cat( "---\n" )
+      stop( "alpha is too high to start the reduction. Please lower the significance value" )
+      cat( "---\n" )
+    } else if ( alpha > pval[ pval_max ] && pval_max != "(Intercept)" && exists( "X_red" ) ) {
+      cat( "---\n" )
+      message( "reduction procedure completed with alpha = ", alpha )
+      cat( "---\n" )
+    }
+    lm.results = matrix( 0, nrow = z$rank, ncol = 4, byrow = TRUE ) 
+    lm.results = cbind( est, se, tval, pval )
+    colnames( lm.results ) = c( "Estimate", "Std. Error", "t value", "Pr(>|t|)" ) 
+    
+    return( list( pval, pval_max ) )
+  }
+  
+  
+  final_fit = function( obj ) {
+    digits = 5
+    z = obj
+    r = z$residuals
+    n = length( r )
+    p = z$rank
+    rss = sum( r^2 )
+    rdf = z$df.residual
+    resvar = rss / rdf
+    sigma = sqrt( resvar )
+    f = z$fitted.values
+    Qr = z$qr
+    p1 = 1L:p
+    R = chol2inv( Qr$qr[ p1, p1, drop = FALSE ] )
+    if( any( names( z$coefficients ) %in% "(Intercept)" ) ) {
+      mss = sum( ( f - mean( f ) )^2 ) 
+      df.int = 1L
+    } else {
+      mss = sum( f^2 )
+      df.int = 0L
+    }
+    r.squared = mss / ( mss + rss )
+    adj.r.squared = 1 - ( 1 - r.squared ) * ( ( n - df.int ) / rdf )
+    fstatistic = c( value = ( mss / ( p - df.int ) ) / resvar, 
+                    numdf = p - df.int, 
+                    dendf = rdf )
+    se = sqrt( diag( R ) * resvar )
+    est = z$coefficients[ Qr$pivot[ p1 ] ]
+    tval = est/se
+    pval = 2 * pt( abs( tval ), rdf, lower.tail = FALSE )
+    lm.results = matrix( 0, nrow = z$rank, ncol = 4, byrow = TRUE ) 
+    lm.results = cbind( est, se, tval, pval )
+    colnames( lm.results ) = c( "Estimate", "Std. Error", "t value", "Pr(>|t|)" ) 
+    
+    return( invisible( list( coef = lm.results, residuals = r, 
+                             fitted.values = f, sigma = sigma, 
+                             r.squared = r.squared, 
+                             adj.r.squared = adj.r.squared, 
+                             fstatistic = fstatistic,
+                             rdf = rdf ) ) )
+  }
+  
   if ( !intercept ) {
     cat( "---\n" )
     message( "fitting a model without intercept" )
