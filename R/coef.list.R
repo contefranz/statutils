c64a7912be1825ce88574ff2a3ed71b92de4e569
Francesco Grossetti <francesco.grossetti@polimi.it>
2016-12-05 09:32:46 +0100

    exporting coef.list

--- R/coef.list.R
+++ R/coef.list.R
@@ -1,3 +1,9 @@
+#' Extract Model Coefficients
+#' 
+#' @param model A \code{list} as computed by \code{\link{reduction}}.
+#' 
+#' @export
+
 coef.list = function( model ) {
   print( model$coef[ , 1 ] )
 }
